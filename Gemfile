1db976a6165a0cb87c78bb73db9f659be0447b1f
Eric Roos <ericroos@hey.com>
2024-07-29 20:38:25 -0500

    add files

---
+++ Gemfile
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in game_log_analyze.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
+
+gem "rspec", "~> 3.0"
+
+gem "rubocop", "~> 1.21"
+
+gem "thor", "~> 1.3"
---
+++ lib/game_log_analyze.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative "game_log_analyze/version"
+require "thor"
+
+module GameLogAnalyze
+  class Error < StandardError; end
+
+  class CLI < Thor
+    # contents of the Thor class
+  end
+  # Your code goes here...
+end
